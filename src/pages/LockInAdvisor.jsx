11bca9d060645b901109d7fd7d0a67d23534aa9d
cadevass <cade.vassalllo06@gmail.com>
2026-07-06 08:18:22 +0000

    fix: add useEffect import to LockInAdvisor

--- src/pages/LockInAdvisor.jsx
+++ src/pages/LockInAdvisor.jsx
@@ -1,4 +1,4 @@
-import { useState, useMemo } from "react";
+import { useState, useMemo, useEffect } from "react";
 import { Lock, TrendingUp, Zap, Plus, X, RefreshCw } from "lucide-react";
 import { useLocalStorage } from "../hooks/useLocalStorage";
 import { callClaude } from "../utils/api";
